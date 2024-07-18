e5240213996554896ca5bc00726d1612bd7905e7
Essential Randomness <essential.randomn3ss@gmail.com>
2024-07-18 16:43:37 -0700

    get wallpaper switcher buttons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+const modules = import.meta.glob("/wallpapers/**/index.html");
+
+function changeWallpaper(e: Event) {
+  const button = e.target as HTMLButtonElement;
+  const path = button.dataset.path!;
+
+  document.querySelectorAll("iframe").forEach((iframe) => (iframe.src = path));
+}
+
+document.querySelector("#wallpaper-selection")!.innerHTML = Object.keys(modules)
+  .map((wallpaperPath) => {
+    const wallpaperName = wallpaperPath.substring(
+      "/wallpapers/".length,
+      wallpaperPath.length - "/index.html".length
+    );
+    return `<button data-path=${wallpaperPath}>${wallpaperName}</button>`;
+  })
+  .join("\n");
+
+document.querySelectorAll("#wallpaper-selection button").forEach((b) => {
+  b.addEventListener("click", changeWallpaper);
+});
